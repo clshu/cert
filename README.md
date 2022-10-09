b7b4008469ac11d2b193d12a640c54d6a246edd2
clshu <clshu2020@gmail.com>
2022-10-10 01:34:37 +0800

    Add -nodes and add more comments to Makefile.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # How to run it
+## No password
+```
+$ ./gen.sh
+```
+## With password
 - To run the shell script, pass the password as the argument
 - e.g.
 ```
--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@ rm *.pem
 rm *.srl
 # 1. Generate CA's private key and self-signed certificate
 # Remove -nodes to encrypt private key
-openssl req -x509 -newkey rsa:4096 -days 365 -keyout ca-key.pem -out ca-cert.pem -subj "/C=US/ST=California/L=Torrance/O=TVUSA/OU=IT/CN=Long John Silver/emailAddress=longjohnsilver@gmail.com"
+openssl req -x509 -newkey rsa:4096 -nodes -days 365 -keyout ca-key.pem -out ca-cert.pem -subj "/C=US/ST=California/L=Torrance/O=TVUSA/OU=IT/CN=Long John Silver/emailAddress=longjohnsilver@gmail.com"
 
 echo "CA's self-signed certificate"
 echo
@@ -10,7 +10,7 @@ openssl x509 -in ca-cert.pem -noout -text
 
 # 2. Generate web server's private key and certificate signing request(CSR).
 # Remove -nodes to encrypt private key
-openssl req -newkey rsa:4096 -days 365 -keyout server-key.pem -out server-req.pem -subj "/C=US/ST=Origan/L=Portland/O=PC Book/OU=Computer/CN=*.pcbook.com/emailAddress=pcbook@gmail.com"
+openssl req -newkey rsa:4096 -nodes -days 365 -keyout server-key.pem -out server-req.pem -subj "/C=US/ST=Origan/L=Portland/O=PC Book/OU=Computer/CN=*.pcbook.com/emailAddress=pcbook@gmail.com"
 
 # 3. Use CA's private key to sign web server's CSR and get back signed certificate.
 openssl x509 -req -in server-req.pem -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
